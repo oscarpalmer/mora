8ef1d6fec056882efd8a57bccaca360bb8c98ad3
Oscar Palmér <oscar.palmer@botek.se>
2025-05-26 09:41:04 +0200

    0.11.1 - fix isReactive export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export {startBatch, stopBatch} from './batch';
 export {computed, type Computed} from './computed';
 export {effect, type Effect} from './effect';
-export {isComputed, isEffect, isSignal} from './is';
+export {isComputed, isEffect, isReactive, isSignal} from './is';
 export {signal, type Signal} from './signal';
